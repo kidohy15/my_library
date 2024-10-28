5cc8227e0b3eafbbfc085a6257e6a970b4755c52
kidohy15 <kidohy307@gmail.com>
2024-10-29 00:02:07 +0900

    style: navbar 스타일 적용

--- src/stories/Nav.tsx
+++ src/stories/Nav.tsx
@@ -1,28 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './nav.css';
+import "./nav.css";
 
-// export interface BoxProps {
-  // primary?: boolean;
-  // backgroundColor?: string;
-  // size?: 'small' | 'medium' | 'large';
-  // label: string;
-  // onClick?: () => void;
-// }
-
-/** Primary UI component for user interaction */
-// export const Box = ({}: BoxProps) => {
 export const Nav = () => {
-  // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
-      type="button"
-      // className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      // style={{ backgroundColor }}
-      // {...props}
-    >
-      {/* {label} */}
-      테스트1
-    </button>
+    <header>
+      <nav className="nav">
+        <a>HOME</a>
+        <a>BLOG</a>
+        <a>PODCAST</a>
+      </nav>
+    </header>
   );
 };
