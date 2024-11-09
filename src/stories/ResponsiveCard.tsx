1641776dfc4a611af94b2060387cbe4cc721ce61
kidohy15 <kidohy307@gmail.com>
2024-11-09 23:16:16 +0900

    feat: responsive card 컴포넌트 생성

---
+++ src/stories/ResponsiveCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import "./responsiveCard.css";
+
+export const ResponsiveCard = () => {
+  return (
+    <div id="wrap">
+      <div className="container">
+        {/* #1 */}
+        <div className="mycard">
+          <div className="cimg">
+            <img src="src/assets/pic1.jpg" alt="Image" />
+          </div>
+          <div className="cdetail">
+            <h2>Card no1</h2>
+            <p>
+              this is the one of the beautiful image of beautiful nature there
+              we have the oportunity live with such a nice creative
+            </p>
+          </div>
+        </div>
+
+        {/* #2 */}
+        <div className="mycard">
+          <div className="cimg">
+            <img src="src/assets/pic2.webp" alt="Image" />
+          </div>
+          <div className="cdetail">
+            <h2>Card no1</h2>
+            <p>
+              this is the one of the beautiful image of beautiful nature there
+              we have the oportunity live with such a nice creative
+            </p>
+          </div>
+        </div>
+
+        {/* #3 */}
+        <div className="mycard">
+          <div className="cimg">
+            <img src="src/assets/pic3.webp" alt="Image" />
+          </div>
+          <div className="cdetail">
+            <h2>Card no1</h2>
+            <p>
+              this is the one of the beautiful image of beautiful nature there
+              we have the oportunity live with such a nice creative
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
