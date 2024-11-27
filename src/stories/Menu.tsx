ac2b37ad62b1180e292bd2bb04a840504360e58a
kidohy15 <kidohy307@gmail.com>
2024-11-27 23:43:19 +0900

    feat: menu 컴포넌트 생성

---
+++ src/stories/Menu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./menu.css";
+
+export const Menu = () => {
+  return (
+    <body className="Menu_body">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Portifilio</a></li>
+      </ul>
+    </body>
+  );
+};
